08f04dd2b6b05b1c03dcd872ffd8a7625e67823e
myskies <3792535@qq.com>
2015-12-06 13:41:13 +0800

    增加标签及跟类

---
+++ Application/CustomMenu/Model/CustomMenuModel.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace CustomMenu\Model;
+
+use Home\Model\HomeModel;
+
+class CustomMenuModel extends HomeModel
+{
+
+
+
+}
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,8 +1,10 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use CustomMenu\Model\CustomMenuModel;
 class IndexController extends Controller {
-    public function index(){
-        $this->display();
+    public function indexAction(){
+        $CustomMenuM = new CustomMenuModel();
+        dump($CustomMenuM->getLists());
     }
 }
