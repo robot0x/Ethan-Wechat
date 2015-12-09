e56234d523aca30773548b0ee6efe90b6e95c18c
mskies <3792535@qq.com>
2015-12-09 09:47:21 +0800

    table_suffix

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,6 +8,7 @@
 return array(
     'TMPL_TEMPLATE_SUFFIX'  =>  '.tpl',     // 默认模板文件后缀
     'ACTION_SUFFIX'         => 'Action',//设置ACTION前缀为action
+    
     /* 数据缓存设置 */
     'DATA_CACHE_PREFIX' => 'yunzhi_ethan_', // 缓存前缀
     'DATA_CACHE_TYPE'   => 'File', // 数据缓存类型
@@ -17,7 +18,8 @@ return array(
     'DB_NAME'               =>  'ethan_wechat',          // 数据库名
     'DB_USER'               =>  'ethan_wechat',      // 用户名
     'DB_PWD'                =>  'Q25jV6UiCOJAg584VvQllRrvWh0ALDyF',          // 密码
-    'DB_PORT'               =>  '3633',        // 端口
+    'DB_PORT'               =>  '3633',             // 端口
+    'DB_PREFIX'             =>  'ethan_',           // 数据库表前缀
 
     'URL_MODEL'             =>  1, // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
 
