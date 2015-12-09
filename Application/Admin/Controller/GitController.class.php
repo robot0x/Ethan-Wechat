be5e909c2abbcbb251ffed78d1d906be24125a56
mskies <3792535@qq.com>
2015-12-09 10:24:34 +0800

    b

--- Application/Admin/Controller/GitController.class.php
+++ Application/Admin/Controller/GitController.class.php
@@ -5,10 +5,8 @@ Class GitController
 {
 	public function indexAction()
 	{
-		echo "hello git";
 		$path = "/mengyunzhi/www/Ethan-Wechat";
 		chdir($path);
-		passthru("pwd");
 		passthru("git config --global user.name 'myskies'");
 		passthru("git config --global user.email 3792535@qq.com");
 		passthru("git fetch --all");
