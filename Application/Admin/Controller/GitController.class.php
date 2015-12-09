2715410a763dff35e64c0534a04abff7b0fa87d4
mskies <3792535@qq.com>
2015-12-09 10:09:21 +0800

    add

--- Application/Admin/Controller/GitController.class.php
+++ Application/Admin/Controller/GitController.class.php
@@ -9,6 +9,8 @@ Class GitController
 		$path = "/mengyunzhi/www/Ethan-Wechat";
 		chdir($path);
 		passthru("pwd");
+		passthru("git reset --hard");
+		passthru("git checkout -- .");
 		passthru("git pull https://github.com/yunzhiclub/Ethan-Wechat.git master");
 		passthru("pwd");
 	}
