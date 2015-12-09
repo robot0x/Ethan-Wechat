974fe561bf9814c2a9c52315e92bad347d8f4b94
mskies <3792535@qq.com>
2015-12-09 10:42:18 +0800

    delete config

--- Application/Admin/Controller/GitController.class.php
+++ Application/Admin/Controller/GitController.class.php
@@ -7,10 +7,7 @@ Class GitController
 	{
 		$path = "/mengyunzhi/www/Ethan-Wechat";
 		chdir($path);
-		passthru("git config --global user.name 'myskies'");
-		passthru("git config --global user.email 3792535@qq.com");
 		passthru("git pull https://mskies:Liuyan7@github.com/yunzhiclub/Ethan-Wechat.git git");
-		echo "hello";
 		echo "done";
 	}
 }
