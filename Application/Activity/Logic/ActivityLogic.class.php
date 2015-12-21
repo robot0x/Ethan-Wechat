36f84ede16a4d1a811880e9a9109c0b304ad5e8e
anqiang1995 <anqiang1995@qq.com>
2015-12-21 19:49:21 +0800

    Activity模块

---
+++ Application/Activity/Logic/ActivityLogic.class.php
@@ -0,0 +1,63 @@
+<?php
+namespace Activity\Logic;
+
+use Activity\Model\ActivityModel;
+
+class ActivityLogic extends ActivityModel
+{
+	protected $errors = array();   //错误信息
+
+	public function getErrors()
+	{
+		return $this->errors;
+	}
+	public function addList($activity)
+	{
+		try{
+			if($this->creat($activity))
+			{
+				$id=$this->add();
+				return $id;
+			}
+			else
+			{
+				$this->errors[]=$this->getError();
+				return false;
+			}
+		}
+		catch(\Think\Exception $e)
+		{
+			$this->errors[]=$e->getMessage();
+			return false;
+		}
+	}
+	public function saveList($list)
+	{
+		try{
+			if(this->create($list))
+			{
+				$id=$this->save();
+				return $id;
+			}
+			else
+			{
+				$this->errors[]=$this->getError();
+				return false;
+			}
+		}
+		catch(\Think\Exception $e)
+		{
+			$this->errors[]=$e->getMessage();
+			return false;
+		}
+	}
+	public function deleteInfo($id)
+	{
+		$map['id']=$id;
+		$datas=$this->where($map)->delete();
+		return $datas;
+	}
+
+
+
+}
---
+++ Application/Activity/Model/ActivityModel.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Activity\Model;
+
+use Yunzhi\Model\YunzhiModel;
+
+class ActivityModel extends YunzhiModel
+{
+	protected $orderBys = array("weight"=>"asc");   //排序方式
+}
