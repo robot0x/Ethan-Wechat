38897f2de4873fb8bc49dc9a8362d0efac2c081e
litian1218 <1181551049@qq.com>
2015-12-28 19:26:51 +0800

    修改搜索

--- Application/Config/Model/ConfigModel.class.php
+++ Application/Config/Model/ConfigModel.class.php
@@ -6,4 +6,6 @@ use Yunzhi\Model\YunzhiModel;
 class ConfigModel extends YunzhiModel
 {
     protected $orderBys     = array("sort"=>"asc");      //排序字段方式
+    protected $keywords     = "I('get.keywords')";        //查询关键字
+    protected $field        = "name";              //查询字段
 }
