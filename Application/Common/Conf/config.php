b16221afad3a10065f2d2f7132af90017222ea23
mskies <3792535@qq.com>
2015-12-29 08:50:31 +0800

    config format

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -20,14 +20,14 @@ return array(
 
     'URL_MODEL'             =>  1, // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
 
-    'TOKEN'   => 'yunzhi', // TOKEN
-    'APPID'   => 'wxc92a0067c6338cbf', //应用ID
-    'APPSECRET'   => 'bb721eba1ceb506c78f46aa9451e2104', // APPSECRET
+    'TOKEN'                 => 'yunzhi', // TOKEN
+    'APPID'                 => 'wxc92a0067c6338cbf', //应用ID
+    'APPSECRET'             => 'bb721eba1ceb506c78f46aa9451e2104', // APPSECRET
 
     //开启令牌验证
-    'TOKEN_ON'      =>    true,  // 是否开启令牌验证 默认关闭
-    'TOKEN_NAME'    =>    '__hash__',    // 令牌验证的表单隐藏字段名称，默认为__hash__
-    'TOKEN_TYPE'    =>    'md5',  //令牌哈希验证规则 默认为MD5
-    'TOKEN_RESET'   =>    true,  //令牌验证出错后是否重置令牌 默认为true
+    'TOKEN_ON'              =>    true,  // 是否开启令牌验证 默认关闭
+    'TOKEN_NAME'            =>    '__hash__',    // 令牌验证的表单隐藏字段名称，默认为__hash__
+    'TOKEN_TYPE'            =>    'md5',  //令牌哈希验证规则 默认为MD5
+    'TOKEN_RESET'           =>    true,  //令牌验证出错后是否重置令牌 默认为true
 
 );
