cbd42e5bda8589bcb4a8c8f3a6c81fc92943772f
mskies <3792535@qq.com>
2016-01-04 14:13:53 +0800

    注释

--- Application/Customer/Logic/CustomerLogic.class.php
+++ Application/Customer/Logic/CustomerLogic.class.php
@@ -1,14 +1,29 @@
 <?php
+/**
+ * 客户
+ * panjie
+ */
 namespace Customer\Logic;
 
 use Customer\Model\CustomerModel;
 
 class CustomerLogic extends CustomerModel {
 
+	/**
+	 * TO：邓浩洋：
+	 * 	这个写的有问题！
+	 * 1.getLists方法在yunzhiModel中存在.
+	 * 2.select()未分页。有10000条记录，全部返回不成？
+	 */
 	public function getLists(){
 		return $this->select();
 	}
 
+	/**
+	 * 通过用户ID获取记录
+	 * @param  string $id 28位openid
+	 * @return list     
+	 */
 	public function getListById($id)
 	{
 
