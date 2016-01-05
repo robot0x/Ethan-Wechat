5eff46faa86e7a2670d789097cb8a8906d29e97c
liuxiqian <1454179583@qq.com>
2016-01-05 15:26:43 +0800

    修改

--- Application/User/Model/UserModel.class.php
+++ Application/User/Model/UserModel.class.php
@@ -10,26 +10,26 @@ class UserModel extends YunzhiModel
     protected $keywords     = "I('get.keywords')"; //查询关键字
     protected $field        = "name"; //查询字段
 
-    //自动验证
-	protected $_validate = array(
-    array('username', 'require', 'username cannot be empty', 0, 'regex', 1),
-    //用户名不能为空
-    array('username','','Account name already exists！',0,'unique',1),
-    // 在新增的时候验证name字段是否唯一
+ //    //自动验证
+	// protected $_validate = array(
+ //    array('username', 'require', 'username cannot be empty', 0, 'regex', 1),
+ //    //用户名不能为空
+ //    array('username','','Account name already exists！',0,'unique',1),
+ //    // 在新增的时候验证name字段是否唯一
 
-    array('name', 'require', 'name cannot be empty', 0, 'regex', 1),
-    //姓名不能为空
-    array('name','','name already exists！',0,'unique',1),
-    // 在新增的时候验证name字段是否唯一
+ //    array('name', 'require', 'name cannot be empty', 0, 'regex', 1),
+ //    //姓名不能为空
+ //    array('name','','name already exists！',0,'unique',1),
+ //    // 在新增的时候验证name字段是否唯一
 
-    array('phonenumber','/^1[3|4|5|8][0-9]\d{4,8}$/','phonenumber is wrong','0','regex',3),
+ //    array('phonenumber','/^1[3|4|5|8][0-9]\d{4,8}$/','phonenumber is wrong','0','regex',3),
    
-    // array('repassword','password','Incorrect password confirmation',0,'confirm'), 
-    // 验证确认密码是否和密码一致
+ //    // array('repassword','password','Incorrect password confirmation',0,'confirm'), 
+ //    // 验证确认密码是否和密码一致
  
-    array('email','email','email format is not correct',0,''), 
-    // 验证邮箱格式是否正确
-   );
+ //    array('email','email','email format is not correct',0,''), 
+ //    // 验证邮箱格式是否正确
+ //   );
 
 
     /**
