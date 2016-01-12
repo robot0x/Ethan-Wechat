0b4461d12d49fa051a4225981eb4f4dc0acc094c
mskies <3792535@qq.com>
2016-01-12 16:10:05 +0800

    done

--- Application/Introduction/Model/IntroductionModel.class.php
+++ Application/Introduction/Model/IntroductionModel.class.php
@@ -12,7 +12,7 @@ use Yunzhi\Model\YunzhiModel;
 class IntroductionModel extends YunzhiModel
 {
 	protected $_validate = array(
-		array("hotel_phone","^1[0-9]{10}$","the phone num length not 11",3, 'regex'),
+		array("hotel_phone","/^1[0-9]{10}$/","the phone num length not 11",3, 'regex'),//验证手机号码
 		);
 
 }
